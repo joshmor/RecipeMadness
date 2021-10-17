ec41f35d6acb1245a099de83e9076e84744ca08a
joshmor <84875014+joshmor@users.noreply.github.com>
2021-10-17 21:39:08 +0100

    Add files via upload

---
+++ RecipeMadnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeMadnessApp.swift
+//  RecipeMadness
+//
+//  Created by Joshua Morris on 17/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct RecipeMadnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
